e01dfb9c6404a39afc7237db3e59b42de99e13d2
Anjani Tiwari <anjanipawan678@gmail.com>
2026-08-13 16:31:05 +0530

    app.py

--- app.py
+++ app.py
@@ -879,6 +879,15 @@ def main():
             "urls": [
                 "stun:stun.l.google.com:19302"
             ]
+        },
+        {
+            "urls": [
+                "turn:openrelay.metered.ca:80",
+                "turn:openrelay.metered.ca:443",
+                "turn:openrelay.metered.ca:443?transport=tcp"
+            ],
+            "username": "openrelayproject",
+            "credential": "openrelayproject"
         }
     ]
     }
